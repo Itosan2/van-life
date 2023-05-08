a5f10d24adf9ce644850a0e0828c5d7bfd22fa74
YT Choong <e2choong@yahoo.com>
2023-05-08 19:16:44 +0800

    cleanning up

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -1,37 +1,44 @@
-import React from "react"
-import { Link, useParams, useLocation, useLoaderData } from "react-router-dom"
-import { getVans } from "../../api"
+import React from "react";
+/** useLocation to use the state pass by Vans.js */
+import { Link, useParams, useLocation, useLoaderData } from "react-router-dom";
+import { getVans } from "../../api";
 
 export function loader({ params }) {
-    return getVans(params.id)
+  return getVans(params.id);
 }
 
 export default function VanDetail() {
-    const location = useLocation()
-    const van = useLoaderData()
+  // const params = useParams(); // remove as no longer using useEffect
+  const location = useLocation();
+  //  const [van, setVan] = React.useState(null); replaced by useLoaderData()
 
-    const search = location.state?.search || "";
-    const type = location.state?.type || "all";
+  const van = useLoaderData();
 
-    return (
-        <div className="van-detail-container">
-            <Link
-                to={`..${search}`}
-                relative="path"
-                className="back-button"
-            >&larr; <span>Back to {type} vans</span></Link>
+  const search = location.state?.search || "";
+  /** ?.state if location.state not null  */
+  /** alternatively  const search = location.state && location.state.search || "" */
+  const type = location.state?.type || "all";
 
-            <div className="van-detail">
-                <img src={van.imageUrl} />
-                <i className={`van-type ${van.type} selected`}>
-                    {van.type}
-                </i>
-                <h2>{van.name}</h2>
-                <p className="van-price"><span>${van.price}</span>/day</p>
-                <p>{van.description}</p>
-                <button className="link-button">Rent this van</button>
-            </div>
+  return (
+    <div className="van-detail-container">
+      <Link to={`..${search}`} relative="path" className="back-button">
+        &larr; <span>Back to {type} vans</span>
+      </Link>
 
-        </div>
-    )
+      <div className="van-detail">
+        <img src={van.imageUrl} />
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        <h2>{van.name}</h2>
+        <p className="van-price">
+          <span>${van.price}</span>/day
+        </p>
+        <p>{van.description}</p>
+        <button className="link-button">Rent this van</button>
+      </div>
+    </div>
+  );
 }
+
+// const params = useParams();
+/** useLocation hook has absolute path & state {form Vans.js} */
+/** is still maitain even we refresh the page */
